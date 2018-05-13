f4b94d403f0378f2dc2fe9170d41da5dca7585ef
mbmcc <m.mccourry@gmail.com>
2018-05-12 22:16:46 -0700

    On branch master  Changes to be committed: 	new file:   LICENSE 	new file:   README.md 	new file:   hw_Max-Value-Pointer/README.md 	new file:   hw_Max-Value-Pointer/a.out 	new file:   hw_Max-Value-Pointer/main.1 	new file:   hw_Max-Value-Pointer/main.1.cpp 	new file:   hw_Max-Value-Pointer/main.2 	new file:   hw_Max-Value-Pointer/main.2.cpp 	new file:   hw_Max-Value-Pointer/main.cpp
     Changes not staged for commit:
    	deleted:    a.out
    	deleted:    main.1
    	deleted:    main.1.cpp
    	deleted:    main.2
    	deleted:    main.2.cpp
    	deleted:    main.cpp

---
+++ hw_Max-Value-Pointer/README.md
@@ -0,0 +1,20 @@
+# CS131 Wk 6 C++ (computer science 1)
+____
+Please choose one of the following questions for the assignment 6
+
+1. Write a program to find the max of integral values. The program will ask the user to input the 10 values and put them in an array. The output of the program shows a pointer which points to the max value.
+
+
+2. Write a program to delete a specific character from a string by using the pointer. 
+
+For example:
+
+string="book"
+
+Character="o"
+
+Output:"bk"
+___
+In hw_Max_Value_Pointer there is a solution for assignment 1
+
+Matthew McCourry 2018
---
+++ hw_Max-Value-Pointer/main.2.cpp
@@ -0,0 +1,37 @@
+/*///////////////////////////////////////////////
+ * Matthew McCourry (CS131 SPR - 2018)
+ * Project notes, user stories and objectives
+ *
+ * [ ] Write a program to find the max of integer values. 
+ * [ ] The program will ask the user to input the 10 values and put them in an array. 
+ * [ ] The output of the program shows a pointer which points to the max value.
+ //////////////////////////////////////////////*/
+
+
+#include <iostream>
+
+
+int main(){
+    const int count = 10;
+    int numbers[count] = {};
+    int max = 0;
+    int *ptrMax;
+    int location = 0;
+    ptrMax = numbers;
+
+// Get input from the user to fill the 10 values in the array
+    std::cout << "Enter 10 integers \n";
+        for (int &elements : numbers)
+        std::cin >> elements;
+    
+//find max value of the array
+    max = numbers[0]; // assume max is at element 0
+    for (int i=1; i<count; i++){
+      if (numbers[i] > max) {
+        max = numbers[i];
+        ptrMax = &numbers[i];
+        location = i;
+      }
+    }
+    std::cout << "The maximum value is " << *ptrMax << " at location " << location << " in the array, Memory Address " << ptrMax << std::endl;
+}
